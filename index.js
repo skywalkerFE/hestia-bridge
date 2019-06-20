4ae4bec434cf7f719004bd48b1d2c96fdf575bde
hydong <haoyangdong@xiaohongshu.com>
2019-06-20 15:17:03 +0800

    update

---
+++ index.js
@@ -0,0 +1,3 @@
+import hestiaBridge from './packages/mixins/hestia-bridge.js'
+
+export default hestiaBridge
---
+++ packages/mixins/hestia-bridge.js
@@ -0,0 +1,69 @@
+const hestiaBridge = {
+  data: () => ({
+    insideHestia: window.parent !== window.self,
+    hestiaChannel: void 0,
+    hestiaPort: {}
+  }),
+  methods: {
+    initCommunication(project) {
+      if (!this.insideHestia) {
+        onmessage = e => {
+          if (e.ports[0]) {
+            let projectName = e.data.project
+
+            this.hestiaPort[projectName] = e.ports[0]
+            this.hestiaPort[projectName].onmessage = e => {
+              this.onMessage(e, projectName)
+            }
+          }
+        }
+      } else {
+        this.hestiaChannel = new MessageChannel()
+  
+        this.hestiaPort = this.hestiaChannel.port1
+        this.hestiaPort.onmessage = this.onMessage
+        window.parent.postMessage({ project }, '*', [this.hestiaChannel.port2])
+      }
+    },
+    sendMessage(callee, params, project) {
+      let port = project ? this.hestiaPort[project] : this.hestiaPort
+
+      port && port.postMessage && port.postMessage({ callee, params })
+    },
+    onMessage(e, project) {
+      let data = e.data
+  
+      this[data.callee](data.params, project)
+    },
+    routeChange(to) {
+      this.$router.push(to)
+    },
+    formatSideMenu(menu, level) {
+      let res = []
+
+      menu.forEach(x => {
+        x.mini = true
+        x.filled = false
+        x.indentLevel = level ? level * 3 : 3
+        if (x.show === void 0 || x.show()) {
+          let temp = {
+            mini: true,
+            filled: false,
+            collapsed: x.collapsed !== void 0 ? x.collapsed : true,
+            indentLevel: level ? level * 3 : 3,
+            content: x.content || x.name,
+            to: x.to || x.route
+          }
+
+          if (x.sub !== void 0 || x.submenus !== void 0) {
+            temp.sub = this.formatSideMenu(x.sub || x.submenus, level ? level + 1 : 2)
+          }
+          res.push(temp)
+        }
+      })
+      return res
+    }
+  }
+}
+  
+export default hestiaBridge
